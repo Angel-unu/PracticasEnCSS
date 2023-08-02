85959c6d607678cc813c54ed16f6af1e6b7aa77f
Angel-unu <angelsalvaxd@gmail.com>
2023-08-02 15:28:11 -0300

    actualiza

---
+++ Clase 10/JavaScript/pag.js
@@ -0,0 +1,75 @@
+//codigos JS
+
+/*
+comentarios
+en multiples 
+lineas
+*/
+
+//comentarios en tabla
+
+//string - caracteres -> comilla ""´´'' (template literal, bastics acento frances)
+
+console.log('Juan')
+
+console.log("String te permite escribir las letras o sea caracteres")/**/
+
+console.log(`anmi1234`)
+
+//-----number-numeros------
+
+console.log(10.1) //numeros
+
+NaN //->Not a Number -> forma parte del tipo de dato number
+
+console.log(10-"Hola")//tratando de hacer el calculo matematico
+
+// ----boolean-booleanos-----
+
+//true or false
+
+true /*distinto "true"*/ 
+false 
+
+console.log(false)
+
+// consola
+// funcion natuva que creo 35-> console.log()
+
+//ejemplo de la importancia del tido de dato que manejamos
+//suma o concatenacion
+
+console.log(10+20)//numbers -> suma matematica
+console.log("10"+"20");//strings ->concateno, los escribe eno al lado del otro
+
+//null, underfined
+null
+undefined
+
+//--------Variables---------
+
+//crarla - definir una variable
+
+//palabra reservada (pr):var, let, const
+
+//pr nombre valor;
+
+var edad=25;
+var nombrePersona="Marta";
+
+let edad=25;//es mas conplejo que el var no te deja usar el mismo valor de la variable
+
+const nombrePersona="Marta";//no te deja usar el mismo valor y no puedes cambiarlo. Usada para constraseñas y demas
+
+//utilizar
+
+//nombre
+
+console.log(edad+10);
+
+//cambiar el valor de una variable | redefinir la variable
+
+edad=789;
+nombrePersona=true;
+
+console.log(nombrePersona);//Ya no es mas "Marta" ahora es true
