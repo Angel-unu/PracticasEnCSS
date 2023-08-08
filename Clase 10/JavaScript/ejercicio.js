d821a77e3bc1ddb0a0a94f393f46b3ce66c0e7ea
Angel-unu <angelsalvaxd@gmail.com>
2023-08-07 22:59:36 -0300

    actu

---
+++ Clase 10/JavaScript/ejercicio.js
@@ -0,0 +1,35 @@
+let nombre="Angel";
+const apellido="Salva";
+let edad=18;
+const mascota="Perro";
+var numA=1;
+let numB=2;
+let numC=3;
+
+let SumaResul=numA+numB;
+
+let RestaResul=numA-numB-numC;
+
+let ProductoResul=numA*numC;
+
+let numIngresado=prompt("Ingrese un numero: ");
+
+let resultado=numIngresado*numC;
+
+console.log(resultado);
+
+alert("CUIDADO")/*Aterta para el Usuario */
+
+console.log(SumaResul);
+
+console.log(RestaResul);
+
+console.log(ProductoResul);
+
+console.log(nombre);
+
+console.log(apellido);
+
+console.log(edad);
+
+console.log(mascota);
---
+++ Clase 11/clase11.js
@@ -0,0 +1,70 @@
+//Operaciones
+
+//Sumas (+)
+
+let num=2;
+let num2=4;
+let resultado;
+
+resultado=num+num2;
+
+console.log(resultado);
+
+//Resta (-)
+
+let Num=4;
+let Num2=6;
+
+let resultado2=Num-Num2;
+
+console.log(resultado2);
+
+//Multiplicacion ( * )
+
+let Num3=4;
+let Num4=4;
+
+console.log(Num3*Num4);
+
+//Math - Objeto - hermamienta que nos da JS para hacer cosas matematicas mas complejas 
+
+//Division ( / )
+
+let di=4;
+let divi=6;
+let resul;
+
+resul=divi/di;
+
+console.log(resul);
+
+//De comparacion
+
+//mayor (>) Menor (<) ; Mayor igual (>=) - menor (<=) 
+
+//para que sea correcto -> numeros
+//si no tiene el igual no contempla cuando son el mismo numero
+
+console.log(50<20); //true
+
+let letrasNombre="Carla";
+let letrasNombre2="Santiago";
+
+console.log(letrasNombre.length>=letrasNombre2.length);//true
+
+//length -> largo ; herramienta que nos da JS para saber el largo de algo
+//En strings nos devuelve la cantidad de caracteres que tiene.
+//dato.length
+
+console.log(letrasNombre2.length)
+
+
+//Igual
+
+//Distino
+
+//Logico
+
+
+
+
